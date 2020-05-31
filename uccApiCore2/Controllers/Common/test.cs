1b2692b33b935b5253f10fac14e2cfcb12b88304
Sonu Kumar <sonukumar18@hotmail.com>
2020-05-31 18:51:06 +0530

    test file commit

---
+++ uccApiCore2/Controllers/Common/test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uccApiCore2.Controllers.Common
+{
+    public class test
+    {
+    }
+}
