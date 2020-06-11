6fa98758419ec9d400643cd3da5e6119786bf2d7
Kit <deepakrahejain@gmail.com>
2020-06-12 01:58:47 +0530

    commit

---
+++ uccApiCore2.Entities/ProductSizeColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uccApiCore2.Entities
+{
+    public class ProductSizeColor
+    {
+        public int ProductSizeColorId { get; set; } = 0;
+        public int ProductId { get; set; } = 0;
+        public int Qty { get; set; } = 0;
+        public decimal Price { get; set; } = 0;
+        public decimal SalePrice { get; set; } = 0;
+        public bool AvailableSize { get; set; } = false;
+        public bool AvailableColors { get; set; } = false;
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public decimal Discount { get; set; } = 0;
+        public bool DiscountAvailable { get; set; } = false;
+        public int CreatedBy { get; set; } = 0;
+        public int Modifiedby { get; set; } = 0;
+    }
+}
