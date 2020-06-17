3351da06a3cf2189aab93a46eb0d29e36c995688
Sonu Kumar <sonukumar18@hotmail.com>
2020-06-17 20:20:22 +0530

    fabric

---
+++ uccApiCore2.BAL/FabricBAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using uccApiCore2.BAL.Interface;
+using uccApiCore2.Entities;
+using uccApiCore2.Repository.Interface;
+
+namespace uccApiCore2.BAL
+{
+    public class FabricBAL : IFabricBAL
+    {
+        IFabricRepository _FabricRepository;
+
+        public FabricBAL(IFabricRepository FabricRepository)
+        {
+            _FabricRepository = FabricRepository;
+        }
+
+        public Task<List<Fabric>> GetFabric(Fabric obj)
+        {
+            return _FabricRepository.GetFabric(obj);
+        }
+
+        public Task<List<Fabric>> GetAllFabric(Fabric obj)
+        {
+            return _FabricRepository.GetAllFabric(obj);
+        }
+
+        public Task<int> SaveFabric(Fabric obj)
+        {
+            return _FabricRepository.SaveFabric(obj);
+        }
+
+    }
+}
---
+++ uccApiCore2.BAL/Interface/IFabricBAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using uccApiCore2.Entities;
+
+namespace uccApiCore2.BAL.Interface
+{
+    public interface IFabricBAL
+    {
+        Task<List<Fabric>> GetFabric(Fabric obj);
+        Task<List<Fabric>> GetAllFabric(Fabric obj);
+        Task<int> SaveFabric(Fabric obj);
+    }
+}
