87f2f90487ad5dedae21e1a73599d63f7c146436
Sonu Kumar <sonukumar18@hotmail.com>
2020-07-13 17:21:51 +0530

    cart

---
+++ uccApiCore2.Repository/CartRepository.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using uccApiCore2.Entities;
+using uccApiCore2.Repository.Interface;
+using static System.Data.CommandType;
+
+namespace uccApiCore2.Repository
+{
+    public class CartRepository : BaseRepository, ICartRepository
+    {
+        public async Task<int> AddToCart(Cart obj)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@UserId", obj.UserID);
+                parameters.Add("@ProductSizeColorId", obj.ProductSizeColorId);
+                parameters.Add("@Quantity", obj.Quantity);
+                var res = await SqlMapper.ExecuteAsync(con, "p_AddToCart", param: parameters, commandType: StoredProcedure);
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<List<Cart>> DelCartById(Cart obj)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@CartId", obj.CartId);
+                parameters.Add("@UserId", obj.UserID);
+                List<Cart> lst = (await SqlMapper.QueryAsync<Cart>(con, "p_DelCartById", param: parameters, commandType: StoredProcedure)).ToList();
+                return lst;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<List<Cart>> GetCartById(Cart obj)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@UserId", obj.UserID);
+                List<Cart> lst = (await SqlMapper.QueryAsync<Cart>(con, "p_DelCartById", param: parameters, commandType: StoredProcedure)).ToList();
+                return lst;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
