5c8922fd146bcb06748a172629cfcf534836602b
Sonu Kumar <sonukumar18@hotmail.com>
2020-07-13 17:19:30 +0530

    cart

--- uccApiCore2/Startup.cs
+++ uccApiCore2/Startup.cs
@@ -79,6 +79,9 @@ namespace uccApiCore2
 
             services.AddTransient<ILookupTagBAL, LookupTagBAL>();
             services.AddScoped<ILookupTagRepository, LookupTagRepository>();
+
+            services.AddTransient<ICartBAL, CartBAL>();
+            services.AddScoped<ICartRepository, CartRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
