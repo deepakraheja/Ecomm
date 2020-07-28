9220acdcdf44efbb331f9bb385b745652d5ce521
Sonu Kumar <sonukumar18@hotmail.com>
2020-07-28 17:03:45 +0530

    commit

--- uccApiCore2.Entities/ProductSizeSet.cs
+++ uccApiCore2.Entities/ProductSizeSet.cs
@@ -9,7 +9,7 @@ namespace uccApiCore2.Entities
         public int SetNo { get; set; } = 0;
         public int Qty { get; set; } = 1;
         public int SelectedQty { get; set; } = 1;
-        public Boolean IsSelected { get; set; } = true;
+        public Boolean IsSelected { get; set; } = false;
         public string Details { get; set; }
 
         public int ProductSizeId { get; set; } = 0;
