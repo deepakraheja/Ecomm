f1bd7fa178229b56d43d73850407063bd39e9b6d
Sonu Kumar <sonukumar18@hotmail.com>
2020-08-04 15:45:58 +0530

    commit

--- uccApiCore2.Entities/Supplier.cs
+++ uccApiCore2.Entities/Supplier.cs
@@ -7,9 +7,9 @@ namespace uccApiCore2.Entities
     public class Supplier
     {
         public int SupplierID { get; set; } = 0;
-        public string Fname { get; set; }
-        public string MName { get; set; } = "";
-        public string LName { get; set; }
+        public string CompanyName { get; set; }
+        public string Name { get; set; } = "";
+        public string Designation { get; set; }
         public string Address1 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
