fd3201269bd522eb6d488e13b660b35763a8fc1a
Kit <deepakrahejain@gmail.com>
2020-08-08 18:42:17 +0530

    commit

---
+++ uccApiCore2.Entities/Cart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace uccApiCore2.Entities
+{
+    public class Cart
+    {
+        public int CartId { get; set; } = 0;
+        public int UserID { get; set; } = 0;
+        public int ProductSizeColorId { get; set; } = 0;
+        public int ProductSizeId { get; set; } = 0;
+        public int Quantity { get; set; } = 0;
+        public string ProductName { get; set; } = "";
+        public int Price { get; set; } = 0;
+        public int SalePrice { get; set; } = 0;
+        public int Discount { get; set; } = 0;
+        public int Qty { get; set; } = 0;
+        public string FrontImage { get; set; } = "";
+        public int ProductId { get; set; } = 0;
+        public string[] ProductImg { get; set; }
+
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public int SetNo { get; set; } = 0;
+        public string RowID { get; set; }
+    }
+}
